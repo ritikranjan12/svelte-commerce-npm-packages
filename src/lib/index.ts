ce230ff9e5889195dc29d90e05def1670f4cfe05
ritikranjan12 <ritik123453@gmail.com>
2024-02-19 15:29:00 +0530

    initial commit

---
+++ src/lib/index.ts
@@ -0,0 +1,3 @@
+// Reexport your entry components here
+export { default as Footer } from './components/Footer.svelte';
+export {getIdFromYoutubeVideo} from './utils/index'
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Footer from '$lib/components/Footer.svelte';
+</script>
+
+<h1 class="text-red-500">Welcome to your library project</h1>
+<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- <Footer data={{
+    page: 'This is the first footer data',
+    pages: 'This is the second footer data',
+}} /> -->
