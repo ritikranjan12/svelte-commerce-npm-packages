0fbd754e72e824df75a7d7fdca7a85c9c3ad3060
ritikranjan12 <ritik123453@gmail.com>
2024-02-22 11:30:32 +0530

    ui added to npm package

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,3 @@
 // Reexport your entry components here
 export { default as Footer } from './components/Footer.svelte';
+export *  from './ui'
---
+++ src/lib/ui/BackButton.svelte
@@ -0,0 +1,43 @@
+<script>
+	import { goto } from "$app/navigation";
+
+	/**
+	 * @type {any}
+	 */
+	 export let clazz;
+	/**
+	 * @type {any}
+	 */
+	 export let whiteText;
+	/**
+	 * @type {any}
+	 */
+	 export let to;
+
+	 function go() {
+		if (to) {
+			goto(to)
+		} else {
+			goto(history.back() || '/')
+		}
+	}
+</script>
+
+<section class="max-w-max {clazz}">
+	<button
+		class="flex max-w-max transform items-center transition duration-300 hover:-translate-x-1 focus:outline-none
+		{whiteText ? 'text-zinc-200 hover:text-white' : 'text-zinc-800 hover:text-primary-500'}"
+		on:click="{go}">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			class="h-5 w-5"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor">
+			<path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M15 19l-7-7 7-7"
+			></path>
+		</svg>
+
+		<span>Back</span>
+	</button>
+</section>
